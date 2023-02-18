ad0210071f40b13c444a171cb78fb9328a71d031
Daniil Tkachenko <dtkachenko41@gmail.com>
2023-02-18 14:12:11 +0400

    added routes for uploading, removing files and get info about files

--- routes/file/file.controller.js
+++ routes/file/file.controller.js
@@ -17,14 +17,59 @@ module.exports = {
     const extension = getFileExtension(originalname);
     const loadedAt = new Date().toISOString();
 
-    const filename = `${fileFolderName}/${id}.${extension}`;
-    await fs.writeFile(filename, buffer, 'binary')
+    const filePath = `${fileFolderName}/${id}.${extension}`;
+    await fs.writeFile(filePath, buffer, 'binary')
 
     const query = `INSERT INTO files (id, name, extension, mime, size, loaded_at)
                       values ('${id}', '${originalname}', '${extension}', '${mimetype}',
                         ${size}, '${loadedAt}')`;
     await createSqlQueryTemplate(query);
 
-    res.status(200).send('uploadFile');
+    res.sendStatus(200);
+  },
+  async getFileInfo(req, res) {
+    const { id } = req.params;
+    const query = `SELECT * FROM files where id = '${id}'`;
+    const { result } = await createSqlQueryTemplate(query);
+    res.status(200).send({ result });
+  },
+  async updateFile(req, res) {
+    const { id } = req.params;
+
+    const {
+      originalname,
+      mimetype,
+      size,
+      buffer,
+    } = req.file;
+
+    const extension = getFileExtension(originalname);
+    const loadedAt = new Date().toISOString();
+    const filePath = `${fileFolderName}/${id}.${extension}`;
+
+    await fs.writeFile(filePath, buffer, 'binary')
+    
+    const query = `UPDATE files SET name = '${originalname}', extension = '${extension}',
+                    mime = '${mimetype}', size = ${size}, loaded_at = '${loadedAt}'
+                      where id = '${id}'`;
+    await createSqlQueryTemplate(query);
+
+    res.sendStatus(200);
+  },
+  async deleteFile(req, res) {
+    const { id } = req.params;
+
+    const getFileQuery = `SELECT extension FROM files where id = '${id}'`;
+    const { result } = await createSqlQueryTemplate(getFileQuery);
+
+    const extension = result?.[0]?.extension;
+    if (!extension) return res.sendStatus(400);
+    const filePath = `${fileFolderName}/${id}.${extension}`;
+    await fs.unlink(filePath);
+
+    const deleteFileQuery = `DELETE FROM files where id = '${id}'`;
+    await createSqlQueryTemplate(deleteFileQuery);
+
+    res.sendStatus(200);
   },
 };
--- routes/file/file.js
+++ routes/file/file.js
@@ -1,13 +1,30 @@
 const express = require('express');
 const multer = require('multer');
 
-const { uploadFile } = require('./file.controller');
+const {
+  uploadFile,
+  getFileInfo,
+  updateFile,
+  deleteFile,
+} = require('./file.controller');
 
 const router = express.Router();
 const upload = multer();
 
+/* ---------- GET ---------- */
+
+router.get('/:id', getFileInfo);
+
+/* ---------- PUT ---------- */
+
+router.put('/update/:id', upload.single('file'), updateFile);
+
 /* ---------- POST ---------- */
 
 router.post('/upload', upload.single('file'), uploadFile);
+
+/* ---------- DELETE ---------- */
+
+router.delete('/delete/:id', deleteFile);
 
 module.exports = router;
